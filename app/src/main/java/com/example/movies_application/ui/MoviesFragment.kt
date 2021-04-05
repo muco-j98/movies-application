048201ce3382634c688ad1a4fc11393b42598370
Johan Muco <johanmuco98@gmail.com>
2021-04-05 20:36:12 +0300

    added navigation

---
+++ app/src/main/java/com/example/movies_application/ui/MoviesFragment.kt
@@ -0,0 +1,8 @@
+package com.example.movies_application.ui
+
+import androidx.fragment.app.Fragment
+import com.example.movies_application.R
+
+class MoviesFragment: Fragment(R.layout.movies_fragment) {
+
+}
